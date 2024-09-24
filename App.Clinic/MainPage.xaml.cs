790c3acaf95e575dc2612a63ea0d8cc292b6315a
Reece Boulware <144837085+rb20g@users.noreply.github.com>
2024-09-23 23:16:50 -0400

    Added .NET Maui App, App.Clinc, and views folder for PatientManagement

---
+++ App.Clinic/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace App.Clinic
+{
+    public partial class MainPage : ContentPage
+    {
+        //int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void PatientsClicked(object sender, EventArgs e)
+        {
+            Shell.Current.GoToAsync("//Patients");
+            //Current allows us access to one instance even though we don't know how to build an instance of App.Shell
+            //GoToAsync() is how we are going to go from one point on the navigation or visual tree to the next point
+
+            //feature branches should only last a day, should be named with the issue number and a descripter 
+
+        }
+
+    }
+
+}
